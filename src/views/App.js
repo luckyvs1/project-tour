d8c295ec6456b9c5f720e597565d1630d753c346
Tyler Johnson <tylerjohnson@Tylers-MacBook-Pro-2.local>
2020-01-12 03:44:37 -0800

    Fix it real good

--- src/views/App.js
+++ src/views/App.js
@@ -5,14 +5,13 @@ import { Switch, Route, withRouter } from "react-router-dom";
 import { withScriptjs } from 'react-google-maps';
 
 import Icon from '@material-ui/core/Icon';
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import Box from '@material-ui/core/Box'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
 import LocationCardList from './components/LocationCardList';
 import LocationSearchInput from './components/LocationSearchInput';
-import ButtonAppBar from './components/ButtonAppBar';
 import SearchBar from './components/SearchBar';
 import { Container } from '@material-ui/core';
 import ButtonAppBar from './components/ButtonAppBar';
@@ -20,87 +19,10 @@ import LocationSearchMenu from './components/LocationSearchMenu';
 import BottomNavigationFooter from './components/BottomNavigationFooter';
 
 class App extends Component {
-    state = {
-        start: {
-            lat: null,
-            lng: null
-        },
-        end: {
-            lat: null,
-            lng: null,
-        }
-    };
-
-    onLsiSelectStart = (location) => {
-        this.setState({
-            start: {
-                lat: location.lat,
-                lng: location.lng
-            }
-        });
-    }
-
-    onLsiSelectEnd = (location) => {
-        this.setState({
-            end: {
-                lat: location.lat,
-                lng: location.lng
-            }
-        });
-    }
-
-    renderLatLngStart = () => {
-        if(this.state.start.lat || this.state.start.lng) {
-            return (
-                <Box align="center">
-                    <Typography variant="body2">
-                        Latitude: {this.state.start.lat} Longitude: {this.state.start.lng}
-                    </Typography>
-                </Box>
-            );
-        } else {
-            return;
-        }
-    }
-
-    renderLatLngEnd = () => {
-        if(this.state.end.lat || this.state.end.lng) {
-            return (
-                <Box align="center">
-                    <Typography variant="body2">
-                        Latitude: {this.state.end.lat} Longitude: {this.state.end.lng}
-                    </Typography>
-                </Box>
-            );
-        } else {
-            return;
-        }
-    }
+    
 
     render() {
         return (
-            <br />
-                <Box paddingX={10}>
-                    <Card>
-                        <CardContent>
-                            <LocationSearchInput
-                                question="Where should your tour start?" 
-                                label="Search"
-                                onChange={this.onLsiSelectStart}
-                            />
-                            {this.renderLatLngStart()}
-                            <br/>
-                            <LocationSearchInput
-                                question="Where should your tour end?" 
-                                label="Search"
-                                onChange={this.onLsiSelectEnd}
-                            />
-                            {this.renderLatLngEnd()}
-                            <LocationCardList />
-                        </CardContent>
-                    </Card>
-                </Box>
-            </Box>
             <div>
                 <ButtonAppBar menuTitle="Adventure Advisor" menuSubTitle="The Travel Plan Engine"/>
                 <Switch>
--- src/views/components/LocationSearchMenu.js
+++ src/views/components/LocationSearchMenu.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Box from '@material-ui/core/Box';
 import SearchBar from './SearchBar';
 import LocationCardList from './LocationCardList';
+import LocationSearchInput from './LocationSearchInput';
 
-const LocationSearchMenu = () => (
-    <div style={{padding: '20px 20px 20px 20px'}}>
-        <Card>
-            <CardContent>
-                <SearchBar label="Or pick your own location!" question="Where should your tour start?"/>
-                <LocationCardList/>
-            </CardContent>
-        </Card>
-    </div>
-)
+class LocationSearchMenu extends React.Component {
+    state = {
+        start: {
+            lat: null,
+            lng: null
+        }
+    };
+    
+    onLsiSelectStart = (location) => {
+        this.setState({
+            start: {
+                lat: location.lat,
+                lng: location.lng
+            }
+        });
+    };
+
+    onLsiSelectEnd = (location) => {
+        this.setState({
+            end: {
+                lat: location.lat,
+                lng: location.lng
+            }
+        });
+    };
+        
+    render() {
+        return (
+            <Box paddingX={10} pb={1}>
+                <Card>
+                    <CardContent>
+                        <LocationSearchInput
+                            question="Where should your tour start?" 
+                            label="Search"
+                            onChange={this.onLsiSelectStart}
+                        />
+                        <LocationCardList/>
+                    </CardContent>
+                </Card>
+            </Box>
+        );
+    }
+}
 
 export default LocationSearchMenu;
